06a53427e28617d1cc0acdf83f7736411bcb24fa
Matthewit Dechatech <mdechatech>
2018-05-10 02:39:55 -0500

    Cleanup and gif

--- Assets/CharTween/Examples/CharTweenExampleSimple.cs
+++ Assets/CharTween/Examples/CharTweenExampleSimple.cs
@@ -1,33 +1,35 @@
-﻿using UnityEngine;
-using CharTween;
-using DG.Tweening;
+﻿using DG.Tweening;
 using TMPro;
+using UnityEngine;
 
-[RequireComponent(typeof(TMP_Text))]
-public class CharTweenExampleSimple : MonoBehaviour
+namespace CharTween.Examples
 {
-    private void Start()
+    [RequireComponent(typeof(TMP_Text))]
+    public class CharTweenExampleSimple : MonoBehaviour
     {
-        // Set text
-        var text = GetComponent<TMP_Text>();
-        text.text = "DETERMINATION";
-
-        var tweener = text.GetCharTweener();    
-        for (var i = 0; i < tweener.CharacterCount; ++i)
+        private void Start()
         {
-            // Move characters in a circle
-            var circleTween = tweener.DOCircle(i, 0.05f, 0.5f)
-                .SetEase(Ease.Linear)
-                .SetLoops(-1, LoopType.Restart);
+            // Set text
+            var text = GetComponent<TMP_Text>();
+            text.text = "DETERMINATION";
 
-            // Oscillate character color between yellow and white
-            var colorTween = tweener.DOColor(i, Color.yellow, 0.5f)
-                .SetLoops(-1, LoopType.Yoyo);
+            var tweener = text.GetCharTweener();    
+            for (var i = 0; i < tweener.CharacterCount; ++i)
+            {
+                // Move characters in a circle
+                var circleTween = tweener.DOCircle(i, 0.05f, 0.5f)
+                    .SetEase(Ease.Linear)
+                    .SetLoops(-1, LoopType.Restart);
 
-            // Offset animations based on character index in string
-            var timeOffset = Mathf.Lerp(0, 1, i / (float)(tweener.CharacterCount - 1));
-            circleTween.fullPosition = timeOffset;
-            colorTween.fullPosition = timeOffset;
+                // Oscillate character color between yellow and white
+                var colorTween = tweener.DOColor(i, Color.yellow, 0.5f)
+                    .SetLoops(-1, LoopType.Yoyo);
+
+                // Offset animations based on character index in string
+                var timeOffset = Mathf.Lerp(0, 1, i / (float)(tweener.CharacterCount - 1));
+                circleTween.fullPosition = timeOffset;
+                colorTween.fullPosition = timeOffset;
+            }
         }
     }
 }
