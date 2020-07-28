a35dce01a4520f824b6667acef424abcbbc7d7de
indie-du <output.feverishly@gmail.com>
2020-07-28 10:47:49 +0900

    add SetAnchorPos, GetAnchorPos methods for UGUI

---
+++ Assets/CharTween/Scripts/CharTweener.PropertiesUGUI.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CharTween
+{
+    public partial class CharTweener
+    {
+        public void SetAnchorPos(int charIndex, Vector2 anchoredPosition) {
+            (GetProxyTransform( charIndex ) as RectTransform).anchoredPosition = anchoredPosition;
+        }
+
+        public Vector2 GetAnchorPos(int charIndex) {
+            return proxyTransforms == null ? Vector2.zero : (GetProxyTransform( charIndex ) as RectTransform).anchoredPosition;
+        }
+    }
+}
